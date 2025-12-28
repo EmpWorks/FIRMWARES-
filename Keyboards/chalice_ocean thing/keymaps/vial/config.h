50684537e733affd4039879f44e48c88ee484659
Aman Kumar <94244333+EmpWorks@users.noreply.github.com>
2025-12-28 13:44:12 +0530

    Add files via upload

---
+++ Keyboards/chalice_ocean thing/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xDD, 0xBE, 0x1C, 0x1D, 0x68, 0x88, 0x86, 0x6E}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
